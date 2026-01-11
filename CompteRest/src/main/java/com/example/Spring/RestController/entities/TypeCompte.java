3986f547e4da5d46e58bc0ca11b439f673d71add
hbenziat1318-cloud <h.benziat1318@uca.ac.ma>
2026-01-11 12:53:28 +0100

    First commit - TP9 Architecture Microservice

---
+++ CompteRest/src/main/java/com/example/Spring/RestController/entities/TypeCompte.java
@@ -0,0 +1,5 @@
+package com.example.Spring.RestController.entities;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
---
+++ CompteRest/src/main/java/com/example/Spring/RestController/repositories/CompteRepository.java
@@ -0,0 +1,9 @@
+package com.example.Spring.RestController.repositories;
+
+import com.example.Spring.RestController.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
